7bcefb25a67a2c49e84634a982380d08b49b0a69
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-07-18 10:12:18 +0500

    Add: Transport chiptasi narxi [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Transport_chiptasi_narxi.py
@@ -0,0 +1,21 @@
+t = int(input())
+f = int(input())
+if t == 1:
+    p = 1700
+elif t == 2:
+    p = 1700
+elif t == 3:
+    p = 4000
+else:
+    print("Notogri transport")
+    exit()
+if f == 1:
+        pass
+elif f == 2:
+    p //= 2
+elif f == 3:
+            p = 0
+else:
+    print("Notogri toifa")
+    exit()
+print(p)
