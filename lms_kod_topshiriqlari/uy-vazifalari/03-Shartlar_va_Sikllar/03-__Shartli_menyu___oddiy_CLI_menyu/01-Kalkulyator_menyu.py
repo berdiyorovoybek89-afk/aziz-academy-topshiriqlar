f9504b95b32fd09499d6556400c21f2e8b956c5a
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-07-21 16:58:53 +0500

    Add: Kalkulyator menyu [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Kalkulyator_menyu.py
@@ -0,0 +1,13 @@
+k = int(input())
+a = int(input())
+b = int(input())
+if k == 1:
+    print(a + b)
+elif k == 2:
+    print(a - b)
+elif k == 3:
+    print(a * b)
+elif k == 4:
+    print(a // b)
+else:
+    print("Notog'ri amal")
