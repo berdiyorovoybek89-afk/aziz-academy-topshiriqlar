e7cb714dc6430d84d19f1a282f3032287b5745ca
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-18 07:57:35 +0500

    Add: 2) Urinishlar sonini sanash [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/02-2__Urinishlar_sonini_sanash.py
@@ -0,0 +1,9 @@
+# Yashirin son = 5
+# Foydalanuvchi to‘g‘ri topguncha kiritadi.
+# Oxirida nechta urinish ketganini chiqaring.
+c = 0
+while True:
+    c += 1
+    if int(input()) == 5:
+        print(c)
+        break
