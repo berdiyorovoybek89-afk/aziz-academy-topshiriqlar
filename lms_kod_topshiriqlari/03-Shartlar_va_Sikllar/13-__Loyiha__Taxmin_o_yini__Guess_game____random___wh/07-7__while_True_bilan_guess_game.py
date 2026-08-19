e4f82d78757b1a2c6c9e235a372162104595cd86
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-19 09:51:28 +0500

    Add: 7) while True bilan guess game [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/07-7__while_True_bilan_guess_game.py
@@ -0,0 +1,12 @@
+# Yashirin son = 9
+# while True ishlating.
+# To‘g‘ri topilganda break qiling.
+while True:
+    n = int(input())
+    if n < 9:
+        print("Low")
+    elif n > 9:
+        print("High")
+    else:
+        print("Correct")
+        break
