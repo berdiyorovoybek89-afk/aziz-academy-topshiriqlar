f13a1a52e4a9c59a876ab36a831e923539096ba5
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-22 08:39:43 +0500

    Add: 10) Cheksiz loop xavfsiz variant [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/15-Infinite_loop_____cheksiz_sikl_muammosi_va_oldini_/10-10__Cheksiz_loop_xavfsiz_variant.py
@@ -0,0 +1,10 @@
+# while True:
+#     tanlov = int(input())
+#     if tanlov == 0: print('Exit'); break
+#     print(tanlov)
+while True:
+    tanlov = int(input())
+    if tanlov == 0:
+        print('Exit')
+        break
+    print(tanlov)
