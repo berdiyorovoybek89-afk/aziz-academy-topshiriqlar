d0c372cc5db7c930f6b575d648c6a2901a87d6a3
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-24 17:36:58 +0500

    Add: 11) pop: index berilgan [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/02-List_metodlari___append__insert__remove__pop__sort/11-11__pop__index_berilgan.py
@@ -0,0 +1,9 @@
+# lst = [10, 20, 30, 40]
+# k beriladi.
+# lst.pop(k) ni chiqaring (olingan element).
+# Keyin listni chiqaring.
+lst = [10, 20, 30, 40]
+k = int(input())
+val = lst.pop(k)
+print(val)
+print(lst)
