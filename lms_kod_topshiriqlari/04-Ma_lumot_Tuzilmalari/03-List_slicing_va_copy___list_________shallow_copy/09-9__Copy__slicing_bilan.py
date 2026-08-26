9fc07ff306fb907b201ce0a7e96f365ba21da508
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-26 10:43:09 +0500

    Add: 9) Copy: slicing bilan [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/03-List_slicing_va_copy___list_________shallow_copy/09-9__Copy__slicing_bilan.py
@@ -0,0 +1,11 @@
+# n = int(input())
+# a = list(map(int, input().split()))
+# b ni slicing bilan copy qiling.
+# b[0] ni 99 ga o'zgartiring.
+# a va b ni alohida chiqaring.
+n = int(input())
+a = list(map(int, input().split()))
+b = a[:]
+b[0] = 99
+print(a)
+print(b)
