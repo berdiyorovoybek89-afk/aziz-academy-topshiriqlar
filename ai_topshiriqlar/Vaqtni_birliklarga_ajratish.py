8d21ee848129fd5abb0fed4b8cd6e0412db10fa4
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-27 09:43:43 +0500

    Add AI task: Vaqtni birliklarga ajratish

---
+++ ai_topshiriqlar/Vaqtni_birliklarga_ajratish.py
@@ -0,0 +1,17 @@
+# Vaqtni birliklarga ajratish
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+T = int(input())
+kun = T // 86400
+qoldiq = T % 86400
+soat = qoldiq // 3600
+qoldiq = qoldiq % 3600
+daqiqa = qoldiq // 60
+sekunt = qoldiq % 60
+print(kun)
+print(soat)
+print(daqiqa)
+print(sekunt)
