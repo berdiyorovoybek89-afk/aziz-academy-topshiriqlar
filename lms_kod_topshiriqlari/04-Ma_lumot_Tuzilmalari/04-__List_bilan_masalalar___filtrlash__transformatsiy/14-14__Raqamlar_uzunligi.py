f50cd5ce904beac08a272eaecdd2148e26df5c18
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-27 08:23:53 +0500

    Add: 14) Raqamlar uzunligi [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/04-__List_bilan_masalalar___filtrlash__transformatsiy/14-14__Raqamlar_uzunligi.py
@@ -0,0 +1,6 @@
+# n va n ta son beriladi.
+# Har bir son nechta raqamdan iboratligini list qilib chiqaring.
+n = int(input())
+numbers = input().split()
+result = [len(num) for num in numbers]
+print(result)
