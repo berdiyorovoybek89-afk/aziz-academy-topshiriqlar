c1ba1742e7a0521d1e9a24fb07754d6db3f3bbef
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-08-27 09:01:39 +0500

    Add AI task: Davomiylikni hisoblash

--- ai_topshiriqlar/Davomiylikni_hisoblash.py
+++ ai_topshiriqlar/Davomiylikni_hisoblash.py
@@ -1,8 +1,17 @@
 # Davomiylikni hisoblash
 # Kurs: Dasturlash / IT
-# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
 # Ball: 100
 # Aziz Academy — AI Topshiriq
 
 # Yechimingizni shu yerga yozing
 # Kirish: input(), chiqish: print()
+h1 = int(input())
+m1 = int(input())
+h2 = int(input())
+m2 = int(input())
+total_minutes = (h2 * 60 + m2) - (h1 * 60 + m1)
+hours = total_minutes // 60
+minutes = total_minutes % 60
+print(hours)
+print(minutes)
