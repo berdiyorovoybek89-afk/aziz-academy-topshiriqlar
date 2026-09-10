1337351acbe4c597e3c8ae0a2dbcd2e5563a0632
berdiyorovoybek89-afk <berdiyorovoybek89@gmail.com>
2026-09-10 21:36:26 +0500

    Add: 5) Qiymatlarni chiqarish [berdiyorovoybek89-afk]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/08-Dict_metodlari___keys____values____items____get__/05-5__Qiymatlarni_chiqarish.py
@@ -0,0 +1,10 @@
+# n = int(input())
+# d = {}
+# for _ in range(n):
+#     k, v = input().split()
+#     d[k] = int(v)
+# qiymatlarni chiqaring
+n = int(input())
+for _ in range(n):
+    k, v = input().split()
+    print(v)
